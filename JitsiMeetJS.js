e3d0ff7daccc0b6f3137f2a621a8de3bddc6b1e4
ovvn <omazinov@gmail.com>
2020-10-26 13:59:51 +0300

    Test.

--- JitsiMeetJS.js
+++ JitsiMeetJS.js
@@ -41,16 +41,9 @@ import { createGetUserMediaEvent } from './service/statistics/AnalyticsEvents';
 
 //socket client
 import io from 'socket.io-client';
+const socket = io.connect('https://modulate.dmapper.co/', { rejectUnauthorized: false, secure: true, transports: ['websocket', 'flashsocket'] });
 
 const logger = Logger.getLogger(__filename);
-
-function toQueryString(paramsObject) {
-    return Object
-        .keys(paramsObject)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(paramsObject[key])}`)
-        .join('&')
-        ;
-}
 
 /**
  * The amount of time to wait until firing
@@ -359,8 +352,6 @@ export default _mergeNamespaceAndModule({
         window.connectionTimes['obtainPermissions.start']
             = window.performance.now();
 
-        const socket = io.connect('https://modulate.dmapper.co/', { rejectUnauthorized: false, secure: true, transports: ['websocket', 'flashsocket'], query: `${toQueryString(window.config)}` });
-
         return RTC.obtainAudioAndVideoPermissions(options)
             .then(tracks => {
                 promiseFulfilled = true;
@@ -374,6 +365,8 @@ export default _mergeNamespaceAndModule({
                 //         getAnalyticsAttributesFromOptions(options)));
 
                 if (!RTC.options.disableAudioLevels) {
+                    let config = window.config;
+                    console.error(config);
                     for (let i = 0; i < tracks.length; i++) {
                         const track = tracks[i];
                         const mStream = track.getOriginalStream();
@@ -414,7 +407,7 @@ export default _mergeNamespaceAndModule({
                                 source.connect(dest);
                                 startAt = Math.max(audioCtx.currentTime, startAt);
                                 startAt += buffer.duration;
-                                source.start(buffer.duration);
+                                source.start(startAt);
                             });
 
                             //replace original stream with modified stream
